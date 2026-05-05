7cd148501797e021e198e054402940fbd2eb8451
Tirth Chaudhary <chaudharytirth23@gmail.com>
2026-05-05 19:33:48 +0530

    fix cors for vercel frontend

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT=process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(cors({
- origin:'http://localhost:5173',
+ origin:['http://localhost:5173','https://react-project-tirth4.vercel.app/app?state=register'],
  credentials:true
 }));
 
