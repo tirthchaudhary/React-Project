24782b5f9ee80552f3e030cf7188cda54455033e
Tirth Chaudhary <chaudharytirth23@gmail.com>
2026-05-05 19:46:25 +0530

    cors replaced

--- server/server.js
+++ server/server.js
@@ -6,12 +6,6 @@ import userRouter from './routes/userRoutes.js';
 import resumeRouter from './routes/resumeRouter.js';
 import aiRouter from './routes/aiRoutes.js';
 
-// database connection
-
- await connectDB();
-
-const app=express();
-const PORT=process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(cors({
@@ -21,6 +15,13 @@ app.use(cors({
   ],
    credentials:true
 }));
+
+
+// database connection
+ await connectDB();
+
+const app=express();
+const PORT=process.env.PORT || 3000;
 
 app.get("/",(req,res)=>res.send("server is live"));
 app.use('/api/users',userRouter);
