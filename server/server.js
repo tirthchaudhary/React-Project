bc9ae392cb8d6bea6f43d5c2967773e81c6db04c
Tirth Chaudhary <chaudharytirth23@gmail.com>
2026-05-06 16:53:59 +0530

    save vercel url

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://react-project-git-main-tirth4.vercel.app'
+    'react-project-tirth4.vercel.app'
   ],
    credentials:true
 }));
