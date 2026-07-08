68f1488ef8568b3418a57f709bd18a4a23f0e4eb
Tirth Chaudhary <chaudharytirth23@gmail.com>
2026-07-08 12:35:31 +0530

    value of token can be extract from the res.cookies

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,17 +1,17 @@
 import jwt from 'jsonwebtoken'
 
- export const protect=async (req,res,next)=>{
-    const token=req.headers.authorization;
-   if(!token){
-    return res.status(401).json({message:"Unauthorized"});
-   }
-   try {
-    const decoded=jwt.verify(token,process.env.JWT_SECRET)
-    req.userId=decoded.userId;
+export const protect = async (req, res, next) => {
+  const token = req.headers.authorization || res.cookies;
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    req.userId = decoded.userId;
     next();
-   } catch (error) {
-     res.status(401).json({message:"Unauthorized"});
-   }
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
 }
 
 
